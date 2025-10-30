31cdc3a5f37d5e64217383ef198173b0bbe11609
Daniel Muñoz Gallardo <dmg00024@gmail.com>
2025-10-30 14:22:37 +0100

    swagger y controladores básicos

--- src/main/java/com/diw/practica/contoller/AdminController.java
+++ src/main/java/com/diw/practica/contoller/AdminController.java
@@ -1,5 +1,16 @@
 package com.diw.practica.contoller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+
+    @GetMapping("/saludo")
+    public String holaUsuario() {
+        return "Hola soy el administrador";
+    }
 }
--- src/main/java/com/diw/practica/contoller/UsuarioController.java
+++ src/main/java/com/diw/practica/contoller/UsuarioController.java
@@ -1,4 +1,15 @@
 package com.diw.practica.contoller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @GetMapping("/saludo")
+    public String holaUsuario() {
+        return "Hola soy un usuario";
+    }
 }
